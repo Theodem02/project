a198a59a06f71d3342f915cace37c8e054410b59
Theodem02 <theophile.demeulier@student.junia.com>
2023-10-28 20:06:06 +0200

    Update app.js

--- app.js
+++ app.js
@@ -8,12 +8,13 @@ const app = express();
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, './uploads');
+        cb(null, path.join(__dirname, 'uploads'));
     },
     filename: (req, file, cb) => {
         cb(null, file.originalname);
     }
 });
+
 
 const upload = multer({ storage: storage });
 
