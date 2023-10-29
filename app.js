cff530c54392f91cd64b171b4e0227b1eb4479b5
Theodem02 <theophile.demeulier@student.junia.com>
2023-10-29 12:35:37 +0100

    update

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const ejs = require('ejs');
 const path = require('path');
 const multer = require('multer');
 const fs = require('fs');
-const axios = require('axios');
 
 const app = express();
 
@@ -16,6 +15,7 @@ const storage = multer.diskStorage({
     }
 });
 
+
 const upload = multer({ storage: storage });
 
 app.engine('html', ejs.__express);
@@ -23,48 +23,32 @@ app.engine('html', ejs.__express);
 app.use(express.static(path.join(__dirname, 'uploads')));
 
 app.set('view engine', 'html');
+
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.post('/uploads', upload.single('fichier'), async (req, res) => {
+app.post('/uploads', upload.single('fichier'), (req, res) => {
     const uploadedFilePath = path.join(__dirname, 'uploads', req.file.originalname);
     console.log(uploadedFilePath);
-
-    // Read the file content
-    fs.readFile(uploadedFilePath, 'utf8', async (err, data) => {
+    fs.readFile(uploadedFilePath, 'utf8', (err, data) => {
         if (err) {
             console.error(err);
             res.send('Error reading the uploaded file.');
         } else {
-            try {
-                // Send the file content to the second container using an HTTP POST request
-                const response = await axios.post('http://file-listener:4000/upload', {
-                    content: data,
-                    filename: req.file.originalname
-                });
-                console.log(response.data);
-
-                // Add a line to print a success message
-                console.log('File successfully uploaded to the second container.');
-
-                res.send(`
-                    <div>
-                        <h2>File uploaded successfully</h2>
-                        <pre>${data}</pre>
-                    </div>
-                    <p><a href="/">Go back to the original page</a></p>
-                `);
-            } catch (error) {
-                console.error(error);
-                res.send('Error sending the file to the second container.');
-            }
+            // Display the text content in a div.
+            res.send(`
+                <div>
+                    <h2>File uploaded successfully</h2>
+                    <pre>${data}</pre>
+                </div>
+                <p><a href="/">Go back to the original page</a></p>
+            `);
         }
     });
 });
-
 
 app.listen(3000, () => {
     console.log('Server started on port 3000');
